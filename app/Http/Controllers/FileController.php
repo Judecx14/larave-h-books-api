3036a1cc0ca4804c256f27432fa85dd652564b97
AngelZapata582 <angelzapata582@gmail.com>
2022-11-16 11:36:39 -0600

    initial commit; subida y descarga de archivos;

---
+++ app/Http/Controllers/FileController.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Http\Controllers;
+use Illuminate\Support\Facades\Storage;
+use Illuminate\Http\Request;
+
+class FileController extends Controller
+{
+    public function formSubmit(Request $request){
+        $request->validate([
+            'images' => 'required|mimes:jpg,jpeg,png,pdf|max:10240'
+         ]);
+
+        try{
+            if($request->hasFile('images')){
+                $image = $request->file('images');
+                $filenameimage = time().'.'.$image->extension();
+                $image->move(storage_path() . '/app/public/images/',$filenameimage);
+                return response()->json(
+                    ['message'=>$filenameimage]
+                );
+            }
+        }catch(Exception $e){
+            return response()->json(
+                ['message'=>$e.getMessage()]
+            );
+        }
+        /*return response()->json(
+            ['succes'=>"ok"]
+        );
+        $fileName = time().'.'.$request->file->getClientOriginalExtension();
+        
+        $request->file->move(public_path('upload'),$fileName);
+        return response()->json(
+            ['succes'=>'archivo subido: '.$fileName]
+        );*/
+    }
+
+    public function downloadImage($name){
+        if(Storage::disk('images')->exists($name)){
+            return Storage::disk('images')->get($name);
+        }
+    }
+
+    public function downloadFile($name){
+        if(Storage::disk('images')->exists($name)){
+            return Storage::disk('images')->download($name);
+        }
+    }
+}
